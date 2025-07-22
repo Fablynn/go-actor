20bf70bd32075f919063d71b7b1f9824f1e5ee65
fablynn <861917988@qq.com>
2025-07-22 11:32:47 +0800

    feat: add rpg data

--- test/rpg_test.go
+++ test/rpg_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"go-actor/common/config"
 	"go-actor/common/config/repository/buffer_config"
+	"go-actor/common/config/repository/skill_config"
 	"testing"
 )
 
@@ -11,5 +12,7 @@ func TestRpg(t *testing.T) {
 		config.InitConfig("./gameconf/data", nil)
 		buffers := buffer_config.LGet()
 		t.Logf("buffers: %v", buffers)
+		skills := skill_config.LGet()
+		t.Logf("skills: %v", skills)
 	})
 }
