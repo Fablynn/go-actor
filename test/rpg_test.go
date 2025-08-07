716c491bd70a555c1cf293395ddbe8f9e501f95d
fablynn <861917988@qq.com>
2025-08-07 16:18:23 +0800

    feat: 添加读嵌套锁与写冲突场景

--- test/rpg_test.go
+++ test/rpg_test.go
@@ -1,10 +1,13 @@
 package test
 
 import (
+	"fmt"
 	"go-actor/common/config"
 	"go-actor/common/config/repository/buffer_config"
-	"go-actor/common/config/repository/skill_config"
+	"go-actor/common/config/repository/skill"
+	"sync"
 	"testing"
+	"time"
 )
 
 func TestRpg(t *testing.T) {
@@ -12,7 +15,40 @@ func TestRpg(t *testing.T) {
 		config.InitConfig("./gameconf/data", nil)
 		buffers := buffer_config.LGet()
 		t.Logf("buffers: %v", buffers)
-		skills := skill_config.LGet()
+		skills := skill.LGet()
 		t.Logf("skills: %v", skills)
 	})
+
+	t.Run("testRwDebule", func(t *testing.T) {
+		go b()
+		go a()
+		go c()
+		time.Sleep(5 * time.Second)
+	})
+}
+
+var rw sync.RWMutex
+
+// 写之前读2
+func a() {
+	rw.RLock()
+	defer rw.RUnlock()
+
+	time.Sleep(1 * time.Second)
+	fmt.Println("aaaa")
+}
+
+func b() {
+	rw.RLock()
+	defer rw.RUnlock()
+	time.Sleep(1 * time.Second)
+	a()
+	fmt.Println("bbbb")
+}
+
+func c() {
+	time.Sleep(1 * time.Second)
+	rw.Lock()
+	defer rw.Unlock()
+	fmt.Println("cccc")
 }
